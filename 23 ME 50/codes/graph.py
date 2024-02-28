bdcdd35436e1b30db3ca8ed8da82797733e7bf17
pragnidhved <142286553+pragnidhved@users.noreply.github.com>
2024-02-28 14:30:48 +0530

    Add files via upload

--- 23 ME 50/codes/graph.py
+++ 23 ME 50/codes/graph.py
@@ -2,15 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Define the range of t values for continuous plot
-t_values = np.linspace(0, 5, 100)  # Adjust the range as per your requirement
-
-# Calculate the corresponding values of e^-2t
-y_values_continuous = np.exp(-2 * t_values)
-
-# Plotting the continuous graph
-plt.plot(t_values, y_values_continuous, label=r'$e^{-2t}$ (continuous)')
-
 data = np.loadtxt('values.dat', delimiter=' ', comments='Values')
+
+# Extracting values for x_1(n) and x_2(n)
+n_values_2 = data[:7, 0]
+x_2_values = data[:7, 1]
+# Plotting the discrete graph
+plt.stem(n_values_2, x_2_values, linefmt='C1-', markerfmt='C1o', basefmt='C1_', label='discrete')
+
+
 
 # Extracting values for x_1(n) and x_2(n)
 n_values_1 = data[:7, 0]
