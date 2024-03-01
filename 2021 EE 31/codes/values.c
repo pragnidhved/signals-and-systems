e9f9b8a096d9980ec4939c35eb457aa3266a3bf1
pragnidhved <142286553+pragnidhved@users.noreply.github.com>
2024-03-01 11:31:57 +0530

    Add files via upload

---
+++ 2021 EE 31/codes/values.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+     double power(double b, int e)
+     {
+     double r=1;
+     int i;
+     for(i=0;i<e;i++)
+     {
+     r*=b;
+     }
+     return r;
+     }
+
+    FILE *file = fopen("values.dat", "w");  // Open a text file for writing
+
+    if (file == NULL) {
+        printf("Error opening the file.\n");
+        return 1;  // Return an error code
+    }
+
+    // Use fprintf to write values to the file directly without arrays
+    fprintf(file, "Values for stem plot of x_1(n):\n");
+    float x[10];
+    for (int i = 0; i < 8; i++) {
+        x[i]=(i+1)*power(0.5,i);
+        fprintf(file, "%d %f\n", i, x[i]);
+    }
+    fclose(file);  // Close the file
+
+    return 0;
+
+}
